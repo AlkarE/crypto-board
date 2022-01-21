b7db20703592f83eed5767fed2cd9d1fdafcd71a
alkar <alkar.ew@gmail.com>
2022-01-21 22:53:14 +0300

    added filter and pagination

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
     <div
       v-if="false"
+      data-spinner
       class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center"
     >
       <svg
@@ -36,39 +37,32 @@
               <input
                 type="text"
                 name="wallet"
+                autocomplete="off"
+                @input="onSearch"
+                v-model="ticker"
+                @keydown.enter="addTicker"
                 id="wallet"
                 class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
                 placeholder="Например DOGE"
               />
             </div>
-            <div
-              class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
-            >
+            <div class="flex bg-white p-1 rounded-md shadow-md flex-wrap">
               <span
+                v-for="c of currencies()"
+                :key="c.id"
+                @click="placeTicker(c)"
                 class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
               >
-                BTC
-              </span>
-              <span
-                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
-              >
-                DOGE
-              </span>
-              <span
-                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
-              >
-                BCH
-              </span>
-              <span
-                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
-              >
-                CHD
+                {{ c.name }}
               </span>
             </div>
-            <div class="text-sm text-red-600">Такой тикер уже добавлен</div>
+            <div v-if="notice" class="text-sm text-red-600">
+              Такой тикер уже добавлен
+            </div>
           </div>
         </div>
         <button
+          @click="addTicker"
           type="button"
           class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         >
@@ -90,102 +84,47 @@
       </section>
 
       <hr class="w-full border-t border-gray-600 my-4" />
+      <div class="p-4">
+        <label for="" class="my-4">
+          filter
+          <input type="text" v-model="filter" />
+        </label>
+        <p>
+          <button
+            @click="onPrevPage"
+            v-if="page > 1"
+            class="my-4 mx-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            Prev
+          </button>
+          <button
+            @click="onNextPage"
+            v-if="hasNextPage"
+            class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            Next
+          </button>
+        </p>
+      </div>
       <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
         <div
+          v-for="t of filteredTickers()"
+          :key="t.id"
           class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
+          :class="selected === t ? 'border-4' : ''"
+          @click="handleSelected(t.id)"
         >
           <div class="px-4 py-5 sm:p-6 text-center">
             <dt class="text-sm font-medium text-gray-500 truncate">
-              WTF - USD
+              {{ t.name }} - USD
             </dt>
-            <dd class="mt-1 text-3xl font-semibold text-gray-900">1.11</dd>
+            <dd class="mt-1 text-3xl font-semibold text-gray-900">
+              {{ t.price }}
+            </dd>
           </div>
           <div class="w-full border-t border-gray-200"></div>
           <button
-            class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
-          >
-            <svg
-              class="h-5 w-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="#718096"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                clip-rule="evenodd"
-              ></path></svg
-            >Удалить
-          </button>
-        </div>
-        <div
-          class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid border-4 cursor-pointer"
-        >
-          <div class="px-4 py-5 sm:p-6 text-center">
-            <dt class="text-sm font-medium text-gray-500 truncate">
-              VUE - RUB
-            </dt>
-            <dd class="mt-1 text-3xl font-semibold text-gray-900">80000.00</dd>
-          </div>
-          <div class="w-full border-t border-gray-200"></div>
-          <button
-            class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
-          >
-            <svg
-              class="h-5 w-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="#718096"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                clip-rule="evenodd"
-              ></path></svg
-            >Удалить
-          </button>
-        </div>
-        <div
-          class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
-        >
-          <div class="px-4 py-5 sm:p-6 text-center">
-            <dt class="text-sm font-medium text-gray-500 truncate">
-              BTC - USD
-            </dt>
-            <dd class="mt-1 text-3xl font-semibold text-gray-900">99999.99</dd>
-          </div>
-          <div class="w-full border-t border-gray-200"></div>
-          <button
-            class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
-          >
-            <svg
-              class="h-5 w-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="#718096"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                clip-rule="evenodd"
-              ></path></svg
-            >Удалить
-          </button>
-        </div>
-        <div
-          class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
-        >
-          <div class="px-4 py-5 sm:p-6 text-center">
-            <dt class="text-sm font-medium text-gray-500 truncate">
-              DOGE - USD
-            </dt>
-            <dd class="mt-1 text-3xl font-semibold text-gray-900">0.0014</dd>
-          </div>
-          <div class="w-full border-t border-gray-200"></div>
-          <button
+            @click="handleDelete(t.id)"
             class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
           >
             <svg
@@ -204,50 +143,253 @@
           </button>
         </div>
       </dl>
-      <hr class="w-full border-t border-gray-600 my-4" />
-      <section class="relative">
-        <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          VUE - USD
-        </h3>
-        <div class="flex items-end border-gray-600 border-b border-l h-64">
-          <div class="bg-purple-800 border w-10 h-24"></div>
-          <div class="bg-purple-800 border w-10 h-32"></div>
-          <div class="bg-purple-800 border w-10 h-48"></div>
-          <div class="bg-purple-800 border w-10 h-16"></div>
-        </div>
-        <button type="button" class="absolute top-0 right-0">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            xmlns:svgjs="http://svgjs.com/svgjs"
-            version="1.1"
-            width="30"
-            height="30"
-            x="0"
-            y="0"
-            viewBox="0 0 511.76 511.76"
-            style="enable-background: new 0 0 512 512"
-            xml:space="preserve"
+      <template v-if="selected">
+        <hr class="w-full border-t border-gray-600 my-4" />
+
+        <section class="relative">
+          <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
+            {{ selected.name }} - USD
+          </h3>
+          <div class="flex items-end border-gray-600 border-b border-l h-64">
+            <div
+              v-for="(d, index) in normalized()"
+              :key="index"
+              class="bg-purple-800 border w-10"
+              :style="{ height: d + '%' }"
+            ></div>
+          </div>
+          <button
+            @click="removeSelected"
+            type="button"
+            class="absolute top-0 right-0"
           >
-            <g>
-              <path
-                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                fill="#718096"
-                data-original="#000000"
-              ></path>
-            </g>
-          </svg>
-        </button>
-      </section>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              xmlns:svgjs="http://svgjs.com/svgjs"
+              version="1.1"
+              width="30"
+              height="30"
+              x="0"
+              y="0"
+              viewBox="0 0 511.76 511.76"
+              style="enable-background: new 0 0 512 512"
+              xml:space="preserve"
+            >
+              <g>
+                <path
+                  d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
+                  fill="#718096"
+                  data-original="#000000"
+                ></path>
+              </g>
+            </svg>
+          </button>
+        </section>
+      </template>
     </div>
   </div>
 </template>
 
 <script>
+function uid() {
+  return Math.random().toString(16).slice(2)
+}
+const { log } = console
 export default {
   name: 'App',
   components: {},
+  data() {
+    return {
+      ticker: '',
+      tickers: [],
+      selected: null,
+      graph: [],
+      notice: false,
+      filter: '',
+      page: 1,
+      hasNextPage: true,
+    }
+  },
+  list: [],
+  timer: null,
+  methods: {
+    filteredTickers() {
+      // 1 -- 0, 5
+      // 2 -- 6, 11
+      const start = 6 * (this.page - 1)
+      const end = 6 * this.page
+      let filter = this.filter.toUpperCase()
+      let filtered = this.tickers.filter((t) => t.name.includes(filter))
+      if (filtered.length > 6 * this.page) {
+        this.hasNextPage = true
+      } else {
+        this.hasNextPage = false
+      }
+      return filtered.slice(start, end)
+    },
+    onNextPage() {
+      log(this.filteredTickers().length)
+      if (this.hasNextPage) this.page++
+    },
+    onPrevPage() {
+      if (this.page > 1) {
+        this.page -= 1
+      }
+    },
+
+    async placeTicker(ticker) {
+      let exists = false
+      this.tickers.forEach((t) => {
+        if (t.name === ticker.name) {
+          exists = true
+        }
+      })
+      if (!exists) {
+        let price = await this.getCurrency(ticker.name)
+        ticker.price = price.USD
+        this.ticker = ''
+        this.tickers.push(ticker)
+      } else {
+        this.ticker = ticker.name
+        this.notice = true
+      }
+    },
+    async addTicker() {
+      const currency = {
+        id: uid(),
+        name: this.ticker.toUpperCase(),
+        price: '-',
+      }
+      let data = await this.getCurrency(currency.name)
+      currency.price = data.USD
+      this.ticker = ''
+      this.tickers.push(currency)
+      this.saveData()
+    },
+    async getCurrency(currency) {
+      const apiKey =
+        '4d4178e3efd1b7d750ea0da36e54f87cb6ecf00614cc177d9eb9c1dddc4ed216'
+      const url = `https://min-api.cryptocompare.com/data/price?fsym=${currency}&tsyms=USD&api_key=${apiKey}`
+      const r = await fetch(url)
+      return await r.json()
+    },
+    handleDelete(id) {
+      this.tickers = this.tickers.filter((c) => c.id !== id)
+    },
+    handleSelected(id) {
+      this.selected = this.tickers.find((t) => t.id === id)
+      this.graph = []
+      this.fillGraph(id)
+    },
+    removeSelected() {
+      this.selected = null
+      clearInterval(this.$options.timer)
+    },
+
+    //     const normalizeBetweenTwoRanges = (val, minVal, maxVal, newMin, newMax) => {
+    //      return newMin + (val - minVal) * (newMax - newMin) / (maxVal - minVal);
+    //    };
+    // (value - min) * 100 / (max - min)
+    normalized() {
+      if (this.graph.length < 2) return
+      let min = Math.min(...this.graph)
+      let max = Math.max(...this.graph)
+      const newMin = 5
+      const newMax = 100
+      if (min === max) max = max + newMax
+      return this.graph.map((x) => {
+        if (min === max) return x
+        let v = newMin + ((x - min) * (newMax - newMin)) / (max - min)
+        return v
+      })
+    },
+    fillGraph(id) {
+      let ticker = this.tickers.find((t) => t.id === id)
+      let get = this.getCurrency
+      let d
+
+      this.$options.timer = setInterval(async () => {
+        d = await get(ticker.name)
+        this.graph.push(d['USD'])
+      }, 3000)
+    },
+    async getList() {
+      let r = await fetch(
+        'https://min-api.cryptocompare.com/data/all/coinlist?summary=true'
+      )
+      let d = await r.json()
+      log('list')
+      this.$options.list = d.Data
+    },
+    onSearch() {
+      this.notice = false
+    },
+    currencies() {
+      if (this.ticker === '') return []
+      let search = this.ticker.toUpperCase()
+      let found = []
+      let counter = 0
+      let list = this.$options.list
+      for (let c in list) {
+        if (counter > 3) break
+        if (c.includes(search) || list[c].FullName.includes(search)) {
+          found.push({
+            id: list[c].Id,
+            name: c,
+          })
+          counter++
+        }
+      }
+
+      return found
+    },
+    saveData() {
+      localStorage.setItem('tickersData', JSON.stringify(this.tickers))
+    },
+    loadData() {
+      const data = localStorage.getItem('tickersData')
+      if (data) {
+        this.tickers = JSON.parse(data)
+      }
+    },
+  },
+  // const state = { 'page_id': 1, 'user_id': 5 }
+  // const title = ''
+  // const url = 'hello-world.html'
+
+  // history.pushState(state, title, url)
+
+  watch: {
+    filter() {
+      this.page = 1
+      window.history.pushState(
+        null,
+        document.title,
+        `?filter=${this.filter}&page=${this.page}`
+      )
+    },
+    page() {
+      window.history.pushState(
+        null,
+        document.title,
+        `?filter=${this.filter}&page=${this.page}`
+      )
+    },
+  },
+  created() {
+    this.getList()
+    this.loadData()
+
+    const windowData = Object.fromEntries(
+      new URL(window.location).searchParams.entries()
+    )
+    if (windowData.filter) {
+      this.filter = windowData.filter
+    }
+    if (windowData.page) {
+      this.page = windowData.page
+    }
+  },
 }
 </script>
-
-<style src="./assets/app.css"></style>
